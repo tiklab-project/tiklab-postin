3f1de40c9a48abcb438923f8a74400b36c38d6fe
zhangzhihua <zhangzhihua@darthcloud.com>
2021-05-05 14:52:25 +0800

    update

---
+++ darth-apibox-deliver-starter/src/main/java/com/darthcloud/apibox/ApiboxApplication.java
@@ -0,0 +1,30 @@
+package com.darthcloud.apibox;
+
+import com.darthcloud.apibox.annotation.EnableApiboxServer;
+import com.darthcloud.apibox.client.annotation.EnableApiboxClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * DarthProjectApplication
+ */
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
+@PropertySource(value = "classpath:application-${env:local}.properties")
+@EnableApiboxClient
+@EnableApiboxServer
+public class ApiboxApplication {
+
+    public static final Logger logger = LoggerFactory.getLogger(ApiboxApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiboxApplication.class, args);
+    }
+
+}
+
