5a2b71ff95f3e0f9dcbaff775b7591d57dd8ecd2
zhangzhihua <zhangzhihua@darthcloud.com>
2021-07-01 03:34:15 +0800

    update

--- darth-apibox-client/src/main/java/com/darthcloud/apibox/client/config/ApiboxClientAutoConfiguration.java
+++ darth-apibox-client/src/main/java/com/darthcloud/apibox/client/config/ApiboxClientAutoConfiguration.java
@@ -5,13 +5,11 @@ import com.darthcloud.apibox.client.builder.ApiboxBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
-@Configuration
 @Profile({"local"})
+@Configuration
+@ComponentScan({"com.darthcloud.apibox"})
 public class ApiboxClientAutoConfiguration {
 
     private static Logger logger = LoggerFactory.getLogger(ApiboxClientAutoConfiguration.class);
