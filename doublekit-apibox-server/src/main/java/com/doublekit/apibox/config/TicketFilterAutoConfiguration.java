b92f6f82fb89e812862bac7469ebb7bf353d8ecc
zhangzhihua <zhangzhihua@darthcloud.com>
2021-09-30 11:33:00 +0800

    update

--- doublekit-apibox-server/src/main/java/com/doublekit/apibox/config/TicketFilterAutoConfiguration.java
+++ doublekit-apibox-server/src/main/java/com/doublekit/apibox/config/TicketFilterAutoConfiguration.java
@@ -1,8 +1,10 @@
 package com.doublekit.apibox.config;
 
-import com.doublekit.eam.client.config.EamTicketConfig;
-import com.doublekit.eam.client.config.EamTicketConfigBuilder;
-import com.doublekit.eam.client.filter.EamTicketFilter;
+import com.doublekit.eam.client.auth.Authenticator;
+import com.doublekit.eam.client.config.TicketConfig;
+import com.doublekit.eam.client.config.TicketConfigBuilder;
+import com.doublekit.eam.client.filter.TicketFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,21 +12,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TicketFilterAutoConfiguration {
 
+    @Autowired
+    Authenticator authenticator;
+
     @Bean
-    public FilterRegistrationBean ticketConfig(EamTicketConfig ticketConfig) {
-        EamTicketFilter ticketFilter = new EamTicketFilter();
+    public FilterRegistrationBean ticketFilterRegistration(TicketConfig ticketConfig) {
+        TicketFilter ticketFilter = new TicketFilter();
         ticketFilter.setTicketConfig(ticketConfig);
+        ticketFilter.setAuthenticator(authenticator);
 
         FilterRegistrationBean registration = new FilterRegistrationBean();
         registration.setFilter(ticketFilter);
+        registration.setName("ticketFilter");
         registration.addUrlPatterns("/*");
         registration.setOrder(2);
         return registration;
     }
 
     @Bean
-    public EamTicketConfig ticketConfig(){
-        return EamTicketConfigBuilder.instance()
+    public TicketConfig ticketConfig(){
+        return TicketConfigBuilder.instance()
                 .ignoreTypes(new String[]{
                         ".ico",
                         ".jpg",
