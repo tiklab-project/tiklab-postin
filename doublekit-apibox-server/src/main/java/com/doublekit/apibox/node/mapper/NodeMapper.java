71ef284d263a750f69cba0be7cbfc391516aa8ea
zhangzhihua <zhangzhihua@darthcloud.com>
2021-11-03 10:45:09 +0800

    remove node model

--- doublekit-apibox-server/src/main/java/com/doublekit/apibox/node/mapper/NodeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.doublekit.apibox.node.mapper;
-
-import com.doublekit.apibox.node.entity.NodeEntity;
-import com.doublekit.apibox.node.model.Node;
-import com.doublekit.beans.annotation.Mapper;
-
-@Mapper(source = Node.class,target = NodeEntity.class)
-public class NodeMapper {
-}
--- doublekit-apibox-server/src/main/resources/scripts/apibase.sql
+++ doublekit-apibox-server/src/main/resources/scripts/apibase.sql
@@ -10,14 +10,6 @@ CREATE TABLE apibox_category(
         parent_category_id VARCHAR(32),
         sort int
 );
-CREATE TABLE apibox_node(
-        id VARCHAR(32) PRIMARY KEY,
-        name VARCHAR(64) NOT NULL,
-        workspace_id VARCHAR(32),
-        parent_category_id VARCHAR(32),
-        sort int,
-        nodeType int
-);
 CREATE TABLE apibox_environment(
         id VARCHAR(32) PRIMARY KEY,
         name VARCHAR(64) NOT NULL,
