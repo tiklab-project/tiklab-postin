ef888a786944fc79876047a5ca86d8af8a660b63
zhangzhihua <zhangzhihua@darthcloud.com>
2021-11-14 16:45:58 +0800

    add datafly version process

--- doublekit-apibox-server/src/main/java/com/doublekit/apibox/config/ApiboxServerAutoConfiguration.java
+++ doublekit-apibox-server/src/main/java/com/doublekit/apibox/config/ApiboxServerAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.doublekit.apibox.config;
 
 
-import com.doublekit.datafly.starter.annotation.DataFly;
+import com.doublekit.datafly.annotation.DataFly;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
