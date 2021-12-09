7119c4c238a152d6904b66d73151328890f304f0
zhangzhihua <zhangzhihua@darthcloud.com>
2021-12-09 11:13:31 +0800

    refactor project module structruer

--- doublekit-apibox-server/src/test/java/com/doublekit/apibox/config/ApiboxTestAutoConfiguration.java
+++ doublekit-apibox-server/src/test/java/com/doublekit/apibox/config/ApiboxTestAutoConfiguration.java
@@ -1,18 +1,16 @@
 package com.doublekit.apibox.config;
 
 
-import com.doublekit.message.starter.annotation.EnableMessage;
+import com.doublekit.message.annotation.EnableMessage;
 import com.doublekit.privilege.annotation.EnablePrivilegeServer;
 import com.doublekit.user.annotation.EnableUserServer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name="apibox.server.enabled",havingValue = "true")
+@EnableMessage
 @EnableUserServer
 @EnablePrivilegeServer
-@EnableMessage
 @ComponentScan({"com.doublekit.apibox"})
 public class ApiboxTestAutoConfiguration {
 
