dd6025942b9b2dedad2d71d7fec9f3ff79fdadff
zhangzhihua <zhangzhihua@darthcloud.com>
2022-01-26 13:48:41 +0800

    update

--- doublekit-apibox-server/src/test/java/com/doublekit/apibox/config/ApiboxTestAutoConfiguration.java
+++ doublekit-apibox-server/src/test/java/com/doublekit/apibox/config/ApiboxTestAutoConfiguration.java
@@ -1,16 +1,10 @@
 package com.doublekit.apibox.config;
 
 
-import com.doublekit.message.annotation.EnableMessage;
-import com.doublekit.privilege.annotation.EnablePrivilegeServer;
-import com.doublekit.user.annotation.EnableUserServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableMessage
-@EnableUserServer
-@EnablePrivilegeServer
 @ComponentScan({"com.doublekit.apibox"})
 public class ApiboxTestAutoConfiguration {
 
