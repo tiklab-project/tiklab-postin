d3cc623f044c617fcffd79aa417762e800255ff4
zhangzhihua <zhangzhihua@darthcloud.com>
2022-03-20 17:18:20 +0800

    refactor plat/pcs

--- doublekit-apibox-starter/src/main/java/com/doublekit/apibox/ApiboxApplication.java
+++ doublekit-apibox-starter/src/main/java/com/doublekit/apibox/ApiboxApplication.java
@@ -1,14 +1,12 @@
 package com.doublekit.apibox;
 
 import com.doublekit.apibox.annotation.EnableApibox;
-import com.doublekit.eam.client.annotation.EnableEamClient;
-import com.doublekit.eam.server.annotation.EnableEamServer;
+import com.doublekit.eam.starter.annotation.EnableEamServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * ApiboxApplication
