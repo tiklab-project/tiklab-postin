c45e8b24d40d96b10f6917292f8fb549e0fbe7be
zhangzhihua <zhangzhihua@darthcloud.com>
2022-05-12 10:55:18 +0800

    删除portal client/server依赖

--- doublekit-apibox-starter/src/main/java/com/doublekit/apibox/ApiboxAutoConfiguration.java
+++ doublekit-apibox-starter/src/main/java/com/doublekit/apibox/ApiboxAutoConfiguration.java
@@ -6,15 +6,16 @@ import com.doublekit.beans.starter.annotation.EnableBeans;
 import com.doublekit.dal.starter.annotation.EnableDal;
 import com.doublekit.dsm.starter.annotation.EnableDsm;
 import com.doublekit.dss.starter.EnableDss;
+import com.doublekit.eam.starter.EnableEam;
 import com.doublekit.gateway.starter.EnableGateway;
 import com.doublekit.join.starter.annotation.EnableJoin;
 import com.doublekit.licence.EnableLicenceServer;
 import com.doublekit.message.starter.EnableMessage;
 import com.doublekit.pluginx.EnablePluginServer;
-import com.doublekit.portal.EnablePortalServer;
 import com.doublekit.privilege.EnablePrivilegeServer;
 import com.doublekit.rpc.starter.annotation.EnableRpc;
 import com.doublekit.toolkit.EnableToolkitServer;
+import com.doublekit.user.starter.EnableUser;
 import com.doublekit.web.starter.annotation.EnableWeb;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,7 +31,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableMessage
 @EnableGateway
 //pcs
-@EnablePortalServer
+@EnableUser
+@EnableEam
 @EnablePrivilegeServer
 @EnablePluginServer
 @EnableToolkitServer
