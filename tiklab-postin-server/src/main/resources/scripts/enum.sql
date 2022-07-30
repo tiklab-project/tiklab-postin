fbebe555993674f87ffd8e491bb440860145213a
孙现诚 <sunxiancheng@doublekit.net>
2022-07-30 22:21:13 +0800

    config

--- tiklab-postin-server/src/main/resources/scripts/enum.sql
+++ tiklab-postin-server/src/main/resources/scripts/enum.sql
@@ -40,10 +40,8 @@ INSERT INTO `prc_role_function` VALUES ('ca2d83007900f0d9dde9b2a22131ddfd', '3b4
 
 INSERT INTO `mec_message_send_type` VALUES ('942591eeee4a3100f2ea2cb871539c65', '站内信', 'site', NULL);
 
-INSERT INTO `mec_message_template` VALUES ('5ea6acf0883ead208d8e75f031ca93df', '站内信', '8066e0357e4bb181b2f7bc438c36dc29', '942591eeee4a3100f2ea2cb871539c65', '事项变更', 1, '有新的消息！', NULL, '/index/message/{id}');
 INSERT INTO `mec_message_template` VALUES ('5ea6acf0883ead208d8e75f031ca94df', '接口空间创建通知模板', '2ba9b35136183577a6973fbf900d5b4b', '942591eeee4a3100f2ea2cb871539c65', '空间创建通知', 1, '你创建了一个空间！', NULL, '/workspacepage/apis/detail/interface/detail');
 INSERT INTO `mec_message_template` VALUES ('60ea6c910b092a0e68bd5a55fddcdc46', '角色添加成员站内消息模板', '2ba9b35136183577a6973fbf900d5b3b', '942591eeee4a3100f2ea2cb871539c65', '角色通知', 1, '用户${userName} ,邀请${users}用户到\"${roleName}\"角色中。', NULL, '/system/role');
 
 INSERT INTO `mec_message_type` VALUES ('2ba9b35136183577a6973fbf900d5b3b', '角色通知', NULL);
 INSERT INTO `mec_message_type` VALUES ('2ba9b35136183577a6973fbf900d5b4b', '创建空间通知', NULL);
-INSERT INTO `mec_message_type` VALUES ('8066e0357e4bb181b2f7bc438c36dc29', '事项变更', NULL);
