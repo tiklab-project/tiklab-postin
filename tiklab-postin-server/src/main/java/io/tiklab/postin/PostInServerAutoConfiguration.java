118415d7e20ec327872264b0d0da78917d83a894
孙现诚 <sxc442533106_cheng@qq.com>
2023-03-13 15:18:20 +0800

    static

--- tiklab-postin-server/src/main/java/io/tiklab/postin/PostInServerAutoConfiguration.java
+++ tiklab-postin-server/src/main/java/io/tiklab/postin/PostInServerAutoConfiguration.java
@@ -2,12 +2,14 @@ package io.tiklab.postin;
 
 
 import io.tiklab.dsm.model.SQL;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"io.tiklab.postin"})
+@ServletComponentScan({"io.tiklab.postin"})
 public class PostInServerAutoConfiguration {
 
     @Bean
