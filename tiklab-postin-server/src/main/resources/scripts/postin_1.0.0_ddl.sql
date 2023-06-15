d05bc13b404bdf3fbf66a48655e29cd963359ace
孙现诚 <sxc442533106_cheng@qq.com>
2023-06-15 10:11:45 +0800

    迁移

--- tiklab-postin-server/src/main/resources/scripts/postin_1.0.0_ddl.sql
+++ tiklab-postin-server/src/main/resources/scripts/postin_1.0.0_ddl.sql
@@ -645,3 +645,11 @@ CREATE TABLE postin_share(
         update_time timestamp
 );
 
+
+CREATE TABLE postin_api_recent (
+        id VARCHAR(32) PRIMARY KEY,
+        workspace_id VARCHAR(32),
+        apix_id VARCHAR(32),
+        user_id VARCHAR (32),
+        update_time timestamp
+);
