8f78fec5dd9f069d0d46e7f4febce3ac019e69bc
孙现诚 <sxc442533106_cheng@qq.com>
2023-07-12 20:52:34 +0800

    代码修改

--- tiklab-postin-starter/src/main/java/io/tiklab/postin/PostInAutoConfiguration.java
+++ tiklab-postin-starter/src/main/java/io/tiklab/postin/PostInAutoConfiguration.java
@@ -1,23 +1,24 @@
 package io.tiklab.postin;
 
+import io.tiklab.dal.boot.starter.annotation.EnableDal;
+import io.tiklab.dcs.boot.starter.EnableDcs;
+import io.tiklab.dfs.boot.starter.EnableDfs;
+import io.tiklab.dsm.boot.starter.EnableDsm;
+import io.tiklab.dss.boot.starter.EnableDss;
 import io.tiklab.integration.starter.EnableIntegration;
 import io.tiklab.licence.starter.EnableLicenceServer;
 import io.tiklab.messsage.starter.EnableMessage;
 import io.tiklab.postgresql.EnablePostgresql;
 import io.tiklab.postin.client.EnablePostInClient;
-import io.tiklab.dal.starter.annotation.EnableDal;
-import io.tiklab.dcs.starter.EnableDcs;
-import io.tiklab.dfs.starter.EnableDfs;
-import io.tiklab.dsm.starter.annotation.EnableDsm;
-import io.tiklab.dss.starter.EnableDss;
+
 import io.tiklab.eam.starter.EnableEam;
 import io.tiklab.gateway.starter.EnableGateway;
 import io.tiklab.pluginx.starter.EnablePluginServer;
 import io.tiklab.privilege.EnablePrivilegeServer;
-import io.tiklab.rpc.starter.annotation.EnableRpc;
+import io.tiklab.rpc.boot.starter.EnableRpc;
 import io.tiklab.security.stater.EnableSecurity;
 import io.tiklab.todotask.stater.EnableTodoTask;
-import io.tiklab.toolkit.starter.EnableToolkit;
+import io.tiklab.toolkit.boot.starter.EnableToolkit;
 import io.tiklab.user.starter.EnableUser;
 import org.springframework.context.annotation.Configuration;
 
