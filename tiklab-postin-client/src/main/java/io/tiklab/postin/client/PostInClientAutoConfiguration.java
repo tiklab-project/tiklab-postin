a39a479b57542f4cc6fa2dc6aabd311fe4dd3ef6
孙现诚 <sxc442533106_cheng@qq.com>
2023-07-28 17:40:37 +0800

    yaml 文件 更改配置

--- tiklab-postin-client/src/main/java/io/tiklab/postin/client/PostInClientAutoConfiguration.java
+++ tiklab-postin-client/src/main/java/io/tiklab/postin/client/PostInClientAutoConfiguration.java
@@ -1,24 +1,8 @@
 package io.tiklab.postin.client;
 
 
-import io.tiklab.dal.boot.starter.annotation.EnableDal;
-import io.tiklab.dcs.boot.starter.annotation.EnableDcsClient;
-import io.tiklab.dcs.boot.starter.annotation.EnableDcsServer;
-import io.tiklab.eam.boot.starter.annotation.EnableEamClient;
-import io.tiklab.eam.boot.starter.annotation.EnableEamServer;
-import io.tiklab.gateway.boot.starter.annotation.EnableGateway;
-import io.tiklab.licence.boot.starter.annotation.EnableLicenceServer;
-import io.tiklab.messsage.boot.starter.annotation.EnableMessageServer;
-import io.tiklab.plugin.starter.EnablePluginServer;
-import io.tiklab.postgresql.EnablePostgresql;
 import io.tiklab.postin.client.builder.PostInBuilder;
-import io.tiklab.privilege.boot.starter.annotation.EnablePrivilegeServer;
-import io.tiklab.rpc.boot.starter.annotation.EnableRpc;
-import io.tiklab.security.boot.stater.annotation.EnableSecurityServer;
-import io.tiklab.todotask.boot.stater.annotation.EnableTodoTaskServer;
-import io.tiklab.toolkit.boot.starter.annotation.EnableToolkit;
-import io.tiklab.user.boot.starter.annotation.EnableUserClient;
-import io.tiklab.user.boot.starter.annotation.EnableUserServer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,28 +11,6 @@ import org.springframework.context.annotation.*;
 
 @Profile({"local","dev"})
 @Configuration
-@EnableToolkit
-@EnablePostgresql
-//@EnableDfsClient
-//@EnableDfsServer
-//@EnableDssClient
-//@EnableDssServer
-@EnableDcsClient
-@EnableDcsServer
-@EnableDal
-@EnableRpc
-@EnableGateway
-//
-@EnableMessageServer
-@EnableSecurityServer
-@EnableTodoTaskServer
-@EnableUserServer
-@EnableUserClient
-@EnableEamServer
-@EnableEamClient
-@EnablePluginServer
-@EnableLicenceServer
-@EnablePrivilegeServer
 @ComponentScan({"io.tiklab.postin"})
 public class PostInClientAutoConfiguration {
 
