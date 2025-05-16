787ce914b229624b1bbe1c462b032b7d77465108
孙现诚 <68012855+hellothread@users.noreply.github.com>
2025-05-16 16:21:10 +0800

    static

--- tiklab-postin-api/src/main/java/io/tiklab/postin/api/http/document/model/ShareQuery.java
+++ tiklab-postin-api/src/main/java/io/tiklab/postin/api/http/document/model/ShareQuery.java
@@ -18,7 +18,7 @@ public class ShareQuery {
     private String targetType;
 
     @ApiProperty(name ="orderParams",desc = "排序参数")
-    private List<Order> orderParams = OrderBuilders.instance().asc("id").get();
+    private List<Order> orderParams = OrderBuilders.instance().desc("createTime").get();
 
     @ApiProperty(name ="pageParam",desc = "分页参数")
     private Page pageParam = new Page();
