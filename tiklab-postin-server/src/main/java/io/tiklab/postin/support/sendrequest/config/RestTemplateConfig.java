4b00eb23a8e7e777196c56ede50354a471b2a2fe
孙现诚 <68012855+hellothread@users.noreply.github.com>
2025-09-24 09:53:57 +0800

    RestTemplate 添加 @Primary

--- tiklab-postin-server/src/main/java/io/tiklab/postin/support/sendrequest/config/RestTemplateConfig.java
+++ tiklab-postin-server/src/main/java/io/tiklab/postin/support/sendrequest/config/RestTemplateConfig.java
@@ -2,13 +2,16 @@ package io.tiklab.postin.support.sendrequest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean("sendRequestRestTemplate")
+    @Primary
     public RestTemplate sendRequestRestTemplate() {
         HttpComponentsClientHttpRequestFactory factory =
                 new HttpComponentsClientHttpRequestFactory();
